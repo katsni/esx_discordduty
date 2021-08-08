745e3dd8832ce362bfceb4d4ca20b9120038bdb6
kytxla <70921605+kytxla@users.noreply.github.com>
2021-08-08 14:17:56 +0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+Config.Zones = {
+
+    ['police'] = {
+        Coords = vector3(441.27, -981.83, 30.69),
+        Job = "police", "offpolice",
+    },
+
+    ['ambulance'] = {
+        Coords = vector3(307.06, -597.57, 43.28),
+        Job = "ambulance", "offambulance",
+    },
+
+    ['mechanic'] = {
+        Coords = vector3(-345.52, -123.0, 39.01),
+        Job = "mechanic", "offmechanic"
+    }
+}
